6da2965234ef2ee3c13cd8bd1db800ed2458d43e
Brian Ventura <briandagoberto20@hotmail.com>
2023-03-26 17:54:52 -0600

    Cambios 26032023

---
+++ database/migrations/2023_03_26_235000_create_tempsales_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tempsales', function (Blueprint $table) {
+            $table->id();
+            $table->string('acuenta')->nullable();
+            $table->boolean('state')->nullable();
+            $table->decimal('totalamount',5,2)->nullable();
+            $table->string('waytopay')->nullable();
+            $table->date('date');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('typedocument_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('client_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tempsales');
+    }
+};
